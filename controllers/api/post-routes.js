701364489c522dd75f4b961cb165ca2298baf7e3
Rajendra Dhanraj <rajiedhan@Rajendras-MacBook-Pro.local>
2021-03-14 00:25:21 -0500

    typo

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -149,7 +149,7 @@ router.put("/:id", withAuth, (req, res) => {
 });
 
 // delete a post
-router.delete("/:id", withAuth, (req, res) => {
+router.delete("/:id", (req, res) => {
   Post.destroy({
     where: {
       id: req.params.id,
