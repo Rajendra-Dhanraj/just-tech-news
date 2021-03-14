4475b5ce5dd5d65470bbb7374149afb6872cf1a3
Rajendra Dhanraj <rajiedhan@Rajendras-MacBook-Pro.local>
2021-03-13 20:41:28 -0500

    added helpers for date, url and plural

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const sequelize = require("./config/connection");
 const path = require("path");
 //handlebars
 const exphbs = require("express-handlebars");
-const hbs = exphbs.create({});
+const helpers = require("./utils/helpers");
+const hbs = exphbs.create({ helpers });
 // express-session
 const session = require("express-session");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
